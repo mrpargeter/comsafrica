93ce218720c07eedf831f04719c5a687e5f0bab0
Justin Pargeter <justin.pargeter@nyu.edu>
2021-09-02 14:40:15 -0400

    Add files via upload

---
+++ comsafrica_stats.R
@@ -0,0 +1,441 @@
+#############################################################
+#  Rcode Compendium for CoMSAfrica Geneva meetings
+#############################################################
+
+#######################
+# R session information
+#######################
+
+sessionInfo()
+
+#R version 3.6.1 (2019-07-05)
+#Platform: x86_64-apple-darwin15.6.0 (64-bit)
+#Running under: macOS Mojave 10.14.6
+
+#attached base packages:
+#  [1] stats     graphics  grDevices utils     datasets  methods   base     
+
+###############################
+#Print list of package versions
+###############################
+
+ip <- as.data.frame(installed.packages()[,c(1,3:4)])
+rownames(ip) <- NULL
+ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]
+print(ip, row.names=FALSE)
+
+################
+# Notes on package versions
+# We use the scale_color_virdis addition to ggplot
+# Also, the recode function in the 'car' package overwrites 'dplyr's'
+# recode function (the one we use). To stop this from happening
+# make sure that dplyr loads after car
+################
+
+################
+# Detach packages and load libraries
+################
+
+detachAllPackages <- function() {
+      
+      basic.packages <- c("package:stats","package:graphics","package:grDevices","package:utils","package:datasets","package:methods","package:base")
+      
+      package.list <- search()[ifelse(unlist(gregexpr("package:",search()))==1,TRUE,FALSE)]
+      
+      package.list <- setdiff(package.list,basic.packages)
+      
+      if (length(package.list)>0)  for (package in package.list) detach(package, character.only=TRUE)
+      
+}
+
+detachAllPackages()
+
+# Create package list to check for installed packages
+
+library(dplyr)
+
+############################## 
+# Set working directory and load datafile
+##############################
+
+# Set to source directory with relevant .csv datafile provided
+# through the Open Science repository
+getwd()
+
+## Add datasets
+
+comsafrica_data<-read.csv("comsafrica_complete_adjusted.csv")
+
+############################## 
+# Trim/tidy data and subset data for analyses
+##############################
+
+# change column names to lower case
+
+colnames(comsafrica_data) <- tolower(colnames(comsafrica_data))
+
+## subset
+
+comsafrica_data_complete<-comsafrica_data %>%
+      subset(completeness=="complete") %>%
+      select(c(assemblage_code,analyst_id,flake_id,proximal_scars,left_scars,distal_scars,right_scars,
+               mass,maximumdimension,maximumwidth,maximumthickness,techlength,techmaxwidth,techmaxthickness,
+               techwidthprox,techwidthmes,techwidthdist,techthickprox,techthickmes,techthickdist,
+               platfwidth,platfthickimpact,platfthickmid,platfthickmax,edgeplatf_deg))
+
+distinct(comsafrica_data_complete, flake_id, .keep_all = TRUE)
+
+comsafrica_data_complete_condb<-comsafrica_data_complete %>%
+      subset(assemblage_code=="chert_condition_B")
+
+comsafrica_data_complete_conda<-comsafrica_data_complete %>%
+      subset(assemblage_code=="chert_condition_A") #%>%
+      #filter(distinct(comsafrica_data_complete$flake_id))
+
+##### Inter rater visualizations and data analyses
+
+# irr
+
+theta = 0.5
+N = 20
+flips1 <- rbinom(n = N, size = 1, prob = theta)
+flips2 <- rbinom(n = N, size = 1, prob = theta)
+flips3 <- rbinom(n = N, size = 1, prob = theta)
+coins<-cbind(flips1, flips2,flips3)
+
+library(irr)
+
+agree(coins, tolerance=0)
+kappa2(coins)
+
+### repeatability coefficients
+
+library(rptR)
+## Condition b
+# maxdim
+hist(comsafrica_data_complete_condb$maximumdimension)
+comsafrica_maxdim_boot<-rpt(maximumdimension ~ (1 | flake_id), 
+                  grname = "flake_id", 
+                  data = comsafrica_data_complete_condb, 
+                  datatype = "Gaussian", 
+                  nboot = 1000, npermut = 0)
+summary(comsafrica_maxdim_boot)
+plot(comsafrica_maxdim_boot, type = "boot", cex.main = 0.8,main="maximum dimension")
+
+#mass
+comsafrica_mass<-rpt(mass ~ (1 | flake_id), 
+                       grname = "flake_id", 
+                       data = comsafrica_data_complete_condb, 
+                       datatype = "Gaussian", 
+                       nboot = 1000, npermut = 0)
+summary(comsafrica_mass)
+plot(comsafrica_mass, type = "boot", cex.main = 0.8,main="mass")
+
+#flake width
+comsafrica_maxwidth<-rpt(maximumwidth ~ (1 | flake_id), 
+                     grname = "flake_id", 
+                     data = comsafrica_data_complete_condb, 
+                     datatype = "Gaussian", 
+                     nboot = 1000, npermut = 0)
+plot(comsafrica_maxwidth, type = "boot", cex.main = 0.8,main="max width")
+
+#flake max thickness
+comsafrica_maxthick<-rpt(maximumthickness ~ (1 | flake_id), 
+                         grname = "flake_id", 
+                         data = comsafrica_data_complete_condb, 
+                         datatype = "Gaussian", 
+                         nboot = 1000, npermut = 0)
+plot(comsafrica_maxthick, type = "boot", cex.main = 0.8,main="max thickness")
+
+#flake tech length
+comsafrica_techlength<-rpt(techlength ~ (1 | flake_id), 
+                         grname = "flake_id", 
+                         data = comsafrica_data_complete_condb, 
+                         datatype = "Gaussian", 
+                         nboot = 1000, npermut = 0)
+plot(comsafrica_techlength, type = "boot", cex.main = 0.8,main="tech length")
+
+#flake tech max width
+comsafrica_techmaxwidth<-rpt(techmaxwidth ~ (1 | flake_id), 
+                           grname = "flake_id", 
+                           data = comsafrica_data_complete_condb, 
+                           datatype = "Gaussian", 
+                           nboot = 1000, npermut = 0)
+plot(comsafrica_techmaxwidth, type = "boot", cex.main = 0.8,main="tech max width")
+
+#flake tech max thickness
+comsafrica_techmaxthick<-rpt(techmaxthickness ~ (1 | flake_id), 
+                             grname = "flake_id", 
+                             data = comsafrica_data_complete_condb, 
+                             datatype = "Gaussian", 
+                             nboot = 1000, npermut = 0)
+plot(comsafrica_techmaxthick, type = "boot", cex.main = 0.8,main="tech max thickness")
+
+#flake tech width prox
+comsafrica_techwidthprox<-rpt(techwidthprox ~ (1 | flake_id), 
+                             grname = "flake_id", 
+                             data = comsafrica_data_complete_condb, 
+                             datatype = "Gaussian", 
+                             nboot = 1000, npermut = 0)
+plot(comsafrica_techwidthprox, type = "boot", cex.main = 0.8,main="tech width proximal")
+
+#flake tech width mes
+comsafrica_techwidthmes<-rpt(techwidthmes ~ (1 | flake_id), 
+                              grname = "flake_id", 
+                              data = comsafrica_data_complete_condb, 
+                              datatype = "Gaussian", 
+                              nboot = 1000, npermut = 0)
+plot(comsafrica_techwidthmes, type = "boot", cex.main = 0.8,main="tech width medial")
+
+#flake tech width dist
+comsafrica_techwidthdist<-rpt(techwidthdist ~ (1 | flake_id), 
+                             grname = "flake_id", 
+                             data = comsafrica_data_complete_condb, 
+                             datatype = "Gaussian", 
+                             nboot = 1000, npermut = 0)
+plot(comsafrica_techwidthdist, type = "boot", cex.main = 0.8,main="tech width dist")
+
+#flake tech thick prox
+comsafrica_techtechthickprox<-rpt(techthickprox ~ (1 | flake_id), 
+                              grname = "flake_id", 
+                              data = comsafrica_data_complete_condb, 
+                              datatype = "Gaussian", 
+                              nboot = 1000, npermut = 0)
+plot(comsafrica_techtechthickprox, type = "boot", cex.main = 0.8,main="tech thick prox")
+
+#flake tech thick med
+comsafrica_techtechthickmes<-rpt(techthickmes ~ (1 | flake_id), 
+                                  grname = "flake_id", 
+                                  data = comsafrica_data_complete_condb, 
+                                  datatype = "Gaussian", 
+                                  nboot = 1000, npermut = 0)
+plot(comsafrica_techtechthickmes, type = "boot", cex.main = 0.8,main="tech thick medial")
+
+#flake tech thick dist
+comsafrica_techthickdist<-rpt(techthickdist ~ (1 | flake_id), 
+                                 grname = "flake_id", 
+                                 data = comsafrica_data_complete_condb, 
+                                 datatype = "Gaussian", 
+                                 nboot = 1000, npermut = 0)
+plot(comsafrica_techthickdist, type = "boot", cex.main = 0.8,main="tech thick dist")
+
+#flake platform width
+comsafrica_platfwidth<-rpt(platfwidth ~ (1 | flake_id), 
+                              grname = "flake_id", 
+                              data = comsafrica_data_complete_condb, 
+                              datatype = "Gaussian", 
+                              nboot = 1000, npermut = 0)
+plot(comsafrica_platfwidth, type = "boot", cex.main = 0.8,main="platform width")
+
+#flake platform thickness
+comsafrica_platfthickmax<-rpt(platfthickmax ~ (1 | flake_id), 
+                           grname = "flake_id", 
+                           data = comsafrica_data_complete_condb, 
+                           datatype = "Gaussian", 
+                           nboot = 1000, npermut = 0)
+plot(comsafrica_platfthickmax, type = "boot", cex.main = 0.8,main="platform thickness")
+
+#flake EPA
+comsafrica_edgeplatf_deg<-rpt(edgeplatf_deg ~ (1 | flake_id), 
+                              grname = "flake_id", 
+                              data = comsafrica_data_complete_condb, 
+                              datatype = "Gaussian", 
+                              nboot = 1000, npermut = 0)
+plot(comsafrica_edgeplatf_deg, type = "boot", cex.main = 0.8,main="EPA")
+
+## Condition a ###
+
+# maxdim
+comsafrica_maxdim_boot<-rpt(maximumdimension ~ (1 | flake_id), 
+                            grname = "flake_id", 
+                            data = comsafrica_data_complete_conda, 
+                            datatype = "Gaussian", 
+                            nboot = 1000, npermut = 0)
+plot(comsafrica_maxdim_boot, type = "boot", cex.main = 0.8,main="maximum dimension")
+
+#mass
+comsafrica_mass<-rpt(mass ~ (1 | flake_id), 
+                     grname = "flake_id", 
+                     data = comsafrica_data_complete_conda, 
+                     datatype = "Gaussian", 
+                     nboot = 1000, npermut = 0)
+plot(comsafrica_mass, type = "boot", cex.main = 0.8,main="mass")
+
+#flake width
+comsafrica_maxwidth<-rpt(maximumwidth ~ (1 | flake_id), 
+                         grname = "flake_id", 
+                         data = comsafrica_data_complete_conda, 
+                         datatype = "Gaussian", 
+                         nboot = 1000, npermut = 0)
+plot(comsafrica_maxwidth, type = "boot", cex.main = 0.8,main="max width")
+
+#flake max thickness
+comsafrica_maxthick<-rpt(maximumthickness ~ (1 | flake_id), 
+                         grname = "flake_id", 
+                         data = comsafrica_data_complete_conda, 
+                         datatype = "Gaussian", 
+                         nboot = 1000, npermut = 0)
+plot(comsafrica_maxthick, type = "boot", cex.main = 0.8,main="max thickness")
+
+#flake tech length
+comsafrica_techlength<-rpt(techlength ~ (1 | flake_id), 
+                           grname = "flake_id", 
+                           data = comsafrica_data_complete_conda, 
+                           datatype = "Gaussian", 
+                           nboot = 1000, npermut = 0)
+plot(comsafrica_techlength, type = "boot", cex.main = 0.8,main="tech length")
+
+#flake tech max width
+comsafrica_techmaxwidth<-rpt(techmaxwidth ~ (1 | flake_id), 
+                             grname = "flake_id", 
+                             data = comsafrica_data_complete_conda, 
+                             datatype = "Gaussian", 
+                             nboot = 1000, npermut = 0)
+plot(comsafrica_techmaxwidth, type = "boot", cex.main = 0.8,main="tech max width")
+
+#flake tech max thickness
+comsafrica_techmaxthick<-rpt(techmaxthickness ~ (1 | flake_id), 
+                             grname = "flake_id", 
+                             data = comsafrica_data_complete_conda, 
+                             datatype = "Gaussian", 
+                             nboot = 1000, npermut = 0)
+plot(comsafrica_techmaxthick, type = "boot", cex.main = 0.8,main="tech max thickness")
+
+#flake tech width prox
+comsafrica_techwidthprox<-rpt(techwidthprox ~ (1 | flake_id), 
+                              grname = "flake_id", 
+                              data = comsafrica_data_complete_conda, 
+                              datatype = "Gaussian", 
+                              nboot = 1000, npermut = 0)
+plot(comsafrica_techwidthprox, type = "boot", cex.main = 0.8,main="tech width proximal")
+
+#flake tech width mes
+comsafrica_techwidthmes<-rpt(techwidthmes ~ (1 | flake_id), 
+                             grname = "flake_id", 
+                             data = comsafrica_data_complete_conda, 
+                             datatype = "Gaussian", 
+                             nboot = 1000, npermut = 0)
+plot(comsafrica_techwidthmes, type = "boot", cex.main = 0.8,main="tech width medial")
+
+#flake tech width dist
+comsafrica_techwidthdist<-rpt(techwidthdist ~ (1 | flake_id), 
+                              grname = "flake_id", 
+                              data = comsafrica_data_complete_conda, 
+                              datatype = "Gaussian", 
+                              nboot = 1000, npermut = 0)
+plot(comsafrica_techwidthdist, type = "boot", cex.main = 0.8,main="tech width dist")
+
+#flake tech thick prox
+comsafrica_techtechthickprox<-rpt(techthickprox ~ (1 | flake_id), 
+                                  grname = "flake_id", 
+                                  data = comsafrica_data_complete_conda, 
+                                  datatype = "Gaussian", 
+                                  nboot = 1000, npermut = 0)
+plot(comsafrica_techtechthickprox, type = "boot", cex.main = 0.8,main="tech thick prox")
+
+#flake tech thick med
+comsafrica_techtechthickmes<-rpt(techthickmes ~ (1 | flake_id), 
+                                 grname = "flake_id", 
+                                 data = comsafrica_data_complete_conda, 
+                                 datatype = "Gaussian", 
+                                 nboot = 1000, npermut = 0)
+plot(comsafrica_techtechthickmes, type = "boot", cex.main = 0.8,main="tech thick medial")
+
+#flake tech thick dist
+comsafrica_techthickdist<-rpt(techthickdist ~ (1 | flake_id), 
+                              grname = "flake_id", 
+                              data = comsafrica_data_complete_conda, 
+                              datatype = "Gaussian", 
+                              nboot = 1000, npermut = 0)
+plot(comsafrica_techthickdist, type = "boot", cex.main = 0.8,main="tech thick dist")
+
+#flake platform width
+comsafrica_platfwidth<-rpt(platfwidth ~ (1 | flake_id), 
+                           grname = "flake_id", 
+                           data = comsafrica_data_complete_conda, 
+                           datatype = "Gaussian", 
+                           nboot = 1000, npermut = 0)
+plot(comsafrica_platfwidth, type = "boot", cex.main = 0.8,main="platform width")
+
+#flake platform thickness
+comsafrica_platfthickmax<-rpt(platfthickmax ~ (1 | flake_id), 
+                              grname = "flake_id", 
+                              data = comsafrica_data_complete_conda, 
+                              datatype = "Gaussian", 
+                              nboot = 1000, npermut = 0)
+plot(comsafrica_platfthickmax, type = "boot", cex.main = 0.8,main="platform thickness")
+
+#flake EPA
+comsafrica_edgeplatf_deg<-rpt(edgeplatf_deg ~ (1 | flake_id), 
+                              grname = "flake_id", 
+                              data = comsafrica_data_complete_conda, 
+                              datatype = "Gaussian", 
+                              nboot = 1000, npermut = 0)
+plot(comsafrica_edgeplatf_deg, type = "boot", cex.main = 0.8,main="EPA")
+
+#### Testing Condition A vs. Condition B
+
+comsafrica_data_complete_summary<-comsafrica_data_complete %>%
+      group_by(flake_id) %>%
+      summarise_at(c("mass","maximumdimension","maximumwidth","maximumthickness",
+                     "techlength","techmaxwidth","techmaxthickness","techwidthprox","techwidthmes",
+                     "techwidthdist","techthickprox","techthickmes","techthickdist","platfwidth",
+                     "platfthickimpact","platfthickmid","platfthickmax","edgeplatf_deg"),mean)
+
+# Bland altman plots
+
+library(dplyr)
+library(ggplot2)
+
+pine_df <- Loblolly
+
+sample_df <- data.frame(sample_n(pine_df, size = nrow(pine_df) * 0.5) %>%
+                              select(height) %>% 
+                              arrange(desc(height)), 
+                        sample_n(pine_df, size = nrow(pine_df) * 0.5) %>%
+                              select(height) %>%
+                              arrange(desc(height)))
+names(sample_df) <- c("Sample_1", "Sample_2")
+
+sample_df$Avg <- (sample_df$Sample_1 + sample_df$Sample_2) / 2
+sample_df$Dif <- sample_df$Sample_1 - sample_df$Sample_2
+
+ggplot(sample_df, aes(x = Avg, y = Dif)) +
+      geom_point(alpha = 0.5) +
+      geom_hline(yintercept = mean(sample_df$Dif), colour = "blue", size = 0.5) +
+      geom_hline(yintercept = mean(sample_df$Dif) - (1.96 * sd(sample_df$Dif)), colour = "red", size = 0.5) +
+      geom_hline(yintercept = mean(sample_df$Dif) + (1.96 * sd(sample_df$Dif)), colour = "red", size = 0.5) +
+      ylab("Diff. Between Measures") +
+      xlab("Average Measure")
+
+library(tidyr)
+
+wide_comsafrica<- comsafrica_data_complete_condb_maximumdimension %>% spread(analyst_id, maximumdimension) %>%
+      na.omit() %>%
+      mutate(Avg=(`0202a`+`034b1`+`9d812`+db727+r42o8)/5)
+
+# OR
+library(BlandAltmanLeh)
+A <- c(-0.358, 0.788, 1.23, -0.338, -0.789, -0.255, 0.645, 0.506, 
+       0.774, -0.511, -0.517, -0.391, 0.681, -2.037, 2.019, -0.447, 
+       0.122, -0.412, 1.273, -2.165)
+B <- c(0.121, 1.322, 1.929, -0.339, -0.515, -0.029, 1.322, 0.951, 
+       0.799, -0.306, -0.158, 0.144, 1.132, -0.675, 2.534, -0.398, 0.537, 
+       0.173, 1.508, -1.955)
+C <- c(0.121, 1.322, 1.929, -0.339, -0.515, -0.029, 1.322, 0.951, 
+       0.799, -0.306, -0.158, 0.144, 1.132, -0.675, 2.534, -0.398, 0.537, 
+       0.173, 1.508, -1.955)
+
+bland.altman.plot(A, B, main="This is a Bland Altman Plot", xlab="Means", ylab="Differences")
+
+#OR ggplot
+
+pl <- bland.altman.plot(A, B, graph.sys = "ggplot2")
+print(pl)
+
+# correlation matrices
+wide_comsafrica_matrix<-wide_comsafrica %>%
+      select(-c(assemblage_code,flake_id,Avg))
+
+res <- cor(wide_comsafrica_matrix)
+
